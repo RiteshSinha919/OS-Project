8a5c49508505877bae9675b12ee30345fca7156a
Ritesh S. Sinha <60837652+RiteshSinha919@users.noreply.github.com>
2020-04-02 17:39:30 +0530

    Add files via upload
    main block is created and initialization is done

--- BarberProblem.c
+++ BarberProblem.c
@@ -15,3 +15,45 @@ sem_t chair_for_hair_stylist;
 sem_t chair_for_sleeping;
 sem_t waiting_time_for_client;
 
+int finished=0;
+int main(int argc,char *argv[])
+{
+	pthread_t btid;
+	pthread_t tid[max_client];
+	int i,num_client,num_seat;
+	int buff[max_client];
+	printf("Enter te number of clients : \n");
+	scanf("%d",&num_client);
+	printf("Enter te number of seats : \n");
+	scanf("%d",&num_seat);
+	if(num_client > max_client)
+	{
+		printf("Maximum number of clients is %d \n",max_client);
+		system("PAUSE");
+		return 0;
+	}
+	for(i=0;i<max_client;i++)
+	{
+		buff[i]=i;
+	}
+	
+	sem_init(&chair_for_client,0,num_seat);
+	sem_init(&chair_for_hair_stylist,0,1);
+	sem_init(&chair_for_sleeping,0,0);
+	sem_init(&waiting_time_for_client,0,0);
+	
+	pthread_create(&btid,NULL,hair_stylist,NULL);
+	for(i=0;i<num_client;i++)
+	{
+		pthread_create(&tid[i],NULL,client,(void *)&buff[i]);
+	}
+	for(i=0;i<num_client;i++)
+	{
+		pthread_join(tid[i],NULL);
+	}
+	finished=1;
+	sem_post(&chair_for_sleeping);
+	pthread_join(btid,NULL);
+	system("PAUSE");
+	return 0;
+}
